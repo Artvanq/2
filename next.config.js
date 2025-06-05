cf40e3c4df226f90ee188576acb71948276e62ed
Artvanq <121523642+Artvanq@users.noreply.github.com>
2025-06-05 21:46:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        'bufferutil': false,
-        'utf-8-validate': false,
-      };
-    }
+  webpack: (config) => {
+    config.externals.push({
+      'bufferutil': 'commonjs bufferutil',
+      'utf-8-validate': 'commonjs utf-8-validate'
+    });
     return config;
-  },
+  }
 };
