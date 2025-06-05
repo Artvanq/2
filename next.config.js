0db46848cfeb939ede9b9df36b276ae6386d731f
Artvanq <121523642+Artvanq@users.noreply.github.com>
2025-06-05 21:38:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = { reactStrictMode: true };
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'bufferutil': false,
+        'utf-8-validate': false,
+      };
+    }
+    return config;
+  },
+};
